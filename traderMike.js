3012a1ceeec9c80d9c32032eec9dedb1d9e6a8c7
TubbyBrah <michael.t.tsotsos@gmail.com>
2020-06-19 11:19:32 +1000

    push
    initial push

---
+++ traderMike.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementsByClassName('class1');
+//const select = navbar.getElementsByClassName('class1');
+//jbfjksbvkdbv
+function clicked(){
+    console.log("clicked");
+}
+navbar.addEventListener('click', clicked);
