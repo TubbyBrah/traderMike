08abd8c21c9645f145c368bc520925e65138b4fd
TubbyBrah <michael.t.tsotsos@gmail.com>
2020-06-19 11:43:01 +1000

    update

--- traderMike.js
+++ traderMike.js
@@ -3,4 +3,4 @@ const example = document.getElementsByClassName('class1');
 function clicked(){
     console.log("clicked");
 }
-example[0].addEventListener('click', clicked);
+example[1].addEventListener('click', clicked);
